ce20b42a2627ef4ff30d86639eaada961bdd792b
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-10-07 11:58:17 +0300

    Main folder uploaded.

---
+++ WebApplication20240925/Controllers/NumbersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication20240925.Controllers
+{
+    public class NumbersController : Controller
+    {
+        private readonly ILogger<NumbersController> _logger;
+
+        public NumbersController(ILogger<NumbersController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult NumbersToN(int count = 3)
+        {
+            ViewBag.Count = count;
+            return View();
+        }
+    }
+}
---
+++ WebApplication20240925/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication20240925.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
